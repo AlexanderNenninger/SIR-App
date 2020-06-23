edb90f206732e12f32fe725e8539750588c6b182
Alexander Nenninger <alexander@NENNIA.UBUNTU>
2020-06-23 15:35:44 +0200

    remove unnecessary files

--- README.md
+++
@@ -1,8 +0,0 @@
-# SIR-App
-
-# How to use
-
- + Make an new python 3.6 environment
- + `pip install -r requirements.txt`
- + `python index.py`
- + follow instructions on screen
--- modules/events.py
+++
@@ -1,8 +0,0 @@
-class event:
-    def __init__(self, func, terminal=False, direction=0):
-        self.func = func
-        self.terminal = terminal
-        self.direction = direction
-
-    def __call__(self, *args, **kwargs):
-        return self.func(*args, **kwargs)
